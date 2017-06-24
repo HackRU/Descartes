7ac040a78016ef8923422a15a24a8de9cad6eb41
dchen <chendavid96@gmail.com>
2017-06-24 16:49:51 -0700

    committed and pushed on Sat Jun 24 16:49:51 PDT 2017

--- parsing/parser.py
+++ parsing/parser.py
@@ -24,29 +24,17 @@
 #     parser('test_cases/img-001.jpg')
 
 
-import io
-import os
 
-# Imports the Google Cloud client library
-from google.cloud import vision
 
-# Instantiates a client
-vision_client = vision.Client()
+try:
+    import Image
+except ImportError:
+    from PIL import Image
+import pytesseract
 
-# The name of the image file to annotate
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'resources/wakeupcat.jpg')
+pytesseract.pytesseract.tesseract_cmd = '<full_path_to_your_tesseract_executable>'
+# Include the above line, if you don't have tesseract executable in your PATH
+# Example tesseract_cmd: 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract'
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
-    image = vision_client.image(
-        content=content)
-
-# Performs label detection on the image file
-labels = image.detect_labels()
-
-print('Labels:')
-for label in labels:
-    print(label.description)
+print(pytesseract.image_to_string(Image.open('test.png')))
+print(pytesseract.image_to_string(Image.open('test-european.jpg'), lang='fra'))
