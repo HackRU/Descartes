3b45274d9090cb0e0ae5e004725247b0d7710214
dchen <chendavid96@gmail.com>
2017-06-24 16:03:31 -0700

    committed and pushed on Sat Jun 24 16:03:31 PDT 2017

---
+++ parsing/parser.py
@@ -0,0 +1,22 @@
+
+
+
+
+def parser(image_link, web=False):
+    if web:  # if web is True, the image link is a web resource, so we need to download it first
+
+        # download the image from the corresponding link and save it as a temp file
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    parser('test_cases/img-001.jpg')
