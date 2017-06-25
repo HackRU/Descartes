9ae434e93d1b20ec642558ac4045f09de446851e
Carlin Au <carlinau@gmail.com>
2017-06-24 20:46:03 -0700

    Able to run python files from express server

--- compiler/app.js
+++ compiler/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const request = require('request');
+const { exec } = require('child_process');
 
 var app = express();
 
-app.get('/', function(req, res){
+app.get('/', (req, res)=>{
   res.send("HELLOWORLD");
+});
+
+app.get('/file-ready', (req, res)=>{
+  exec('python ../dump/test.py', (err, stdout, stderr)=>{
+    if(stderr) {
+      console.log(stderr);
+    }
+    console.log(stdout);
+    res.status(200);
+  });
 });
 
 app.listen(8080);
