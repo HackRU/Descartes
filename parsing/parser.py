12440eb9e8b260d7b3c1319a9f06968f17d4046f
dchen <chendavid96@gmail.com>
2017-06-25 06:51:51 -0700

    committed and pushed on Sun Jun 25 06:51:51 PDT 2017

--- parsing/parser.py
+++ parsing/parser.py
@@ -1,12 +1,52 @@
 import cv2
 import numpy as np
+from flask import Flask, request, json
 from PIL import ImageEnhance
+import os
+import http.client, urllib.request, urllib.parse, urllib.error, base64, requests, time, json
+from imgurpython import ImgurClient
 
 
+client_id = 'd18cea4af0b4c75'
+client_secret = 'e0dd972784aab4b0b4d39156f982d55c29b4d3b3'
+
+AZURE_SUB_KEY = '99894b2c78f144ca80dc013e744a8da2'
+AZURE_REQ_HEADERS = {
+        # Request headers.
+        # Another valid content type is "application/octet-stream".
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': AZURE_SUB_KEY,
+    }
+
+imgur_client = ImgurClient(client_id, client_secret)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return 'HOME PAGE DUDE.'
+
+@app.route("/upload", methods=['POST'])
+def upload():
+
+    image_file = request.data
+    with open('../dump/temp.jpg', 'wb') as f:
+        f.write(image_file)
+    f.close()
+
+    response = app.response_class(
+        response=json.dumps('received ur img! thx.'),
+        status=200,
+        mimetype='html/text'
+    )
+
+    parse()
+    return response
 
 def parse():
 
-    img_file = 'C:/test_cases/nice.jpg' # load the image
+    img_file = '../dump/temp.jpg' # load the image
     o_img = cv2.cvtColor(cv2.imread(img_file), cv2.COLOR_BGR2GRAY) # convert to grayscale
     o_rows, o_cols = o_img.shape # original image height and width
     new_size_pixels = 500
@@ -28,10 +68,10 @@ def parse():
             if image[r, c] < min_so_far:
                 min_so_far = image[r, c]
 
-        if min_so_far < 50 and not start:
+        if min_so_far < 100 and not start:
             start = True
             start_r = r
-        elif min_so_far > 50 and start and r - start_r > 30:
+        elif min_so_far > 100 and start and r - start_r > 30:
             start = False
             end_r = r
             list_of_sections.append({'start': start_r-10, 'end': end_r+10})
@@ -46,60 +86,110 @@ def parse():
         crop_img = image[line['start']:line['end'], 0:cols]
         list_of_img_dicts.append({'img': crop_img, 'indents': 0})
 
+    last_indent_pos = 0
+    current_indent = 0
+    op_locs = []
 
     for img_num, img in enumerate(list_of_img_dicts):  # loop through to pass to azure
-        cv2.imshow('img {}'.format(img_num), img['img'])
-        # cv2.imwrite('{}.jpg'.format(img_num), crop_img)
+        transposed = img['img'].T
+        hit_text = False
+        for c_num, col in enumerate(transposed):
+            if not hit_text:
+                for row in col:
+                    if row < 100 and not hit_text:
+                        hit_text = True
+                        if (c_num - last_indent_pos) > 75:
+                            current_indent += 1
+                        last_indent_pos = c_num
+                        img['indents'] = current_indent
+                    else:
+                        pass
+            else:
+                break
+
+        print('img-{}'.format(img_num), img['indents'])
+
+        cv2.imshow('img-{}'.format(img_num), img['img'])
+        cv2.imwrite('../dump/img-{}.jpg'.format(img_num), img['img'])
+        r = imgur_client.upload_from_path('../dump/img-{}.jpg'.format(img_num))
+        link_to_img = r['link']
+
+        op_location = send_to_azure(link_to_img)
+        print(op_location)
+        op_locs.append(op_location)
 
 
+    print('All images sent to Azure. Waiting 10 seconds for processing.')
+    time.sleep(10)
 
-    # avg_shades_of_rows = np.average(image, axis=0)
-    # avg_img_shade = np.average(avg_shades_of_rows, axis=0)
-    # print('avg shade of image: {}'.format(avg_img_shade))
+    # after this point, all processing should be complete for every picture we sent above
+    # use the op locs and loop through to retrieve data from their server
 
-    # avg_shades = []
-    # for r in range(rows):
-    #     sum_shade_of_row = 0
-    #     for c in range(cols):
-    #         sum_shade_of_row += image[r,c]
-    #     avg_shade_of_row = sum_shade_of_row / cols
-    #     avg_shades.append(avg_shade_of_row)
-    #
-    # avg_shade = sum(avg_shades)/len(avg_shades)
-    # print(avg_shade)
+    for location in op_locs:
+        # Execute the second REST API call and get the response.
+        response = requests.request('GET', location, json=None, data=None, headers=AZURE_REQ_HEADERS, params=None)
+        # 'data' contains the JSON data. The following formats the JSON data for display.
+        parsed = json.loads(response.text)
+        text_lines = parsed['recognitionResult']['lines']
 
-    # list_of_sections = []
-    # start = False
-    # for r in range(rows):
-    #     avg_shade_of_row = avg_shades[r]
-    #     if avg_shade_of_row < avg_shade and not start:
-    #         start = True
-    #         start_r = r
-    #     elif avg_shade_of_row > avg_shade and start:
-    #         start = False
-    #         end_r = r
-    #         list_of_sections.append({'start': start_r-10, 'end': end_r+10})
-    #     else:
-    #         pass
+        actual_line = ''
+        for line in text_lines:
+            actual_line += (line['text'] + ' ')
 
-
-    #
-    # print(list_of_sections)
-    # for line in list_of_sections:
-    #     cv2.line(image, (0, line['start']), (cols, line['start']), (0, 0, 0), thickness=1)
-    #     cv2.line(image, (0, line['end']), (cols, line['end']), (0, 0, 0), thickness=1)
+        print(actual_line)
+        # print(json.dumps(parsed, sort_keys=True, indent=2))
 
     cv2.imshow('', np.asarray(image))
-    cv2.imwrite('nice.jpg', image)
-
     cv2.waitKey()
 
     return
 
 
+def send_to_azure(i_link):
 
+    ###############################################
+    #### Update or verify the following values. ###
+    ###############################################
 
+    uri_base = 'https://westus.api.cognitive.microsoft.com'
+
+    # The URL of a JPEG image containing handwritten text.
+    body = {'url': i_link}
+    # For printed text, set "handwriting" to false.
+    params = {'handwriting': 'true'}
+
+    try:
+        # This operation requrires two REST API calls. One to submit the image for processing,
+        # the other to retrieve the text found in the image.
+        #
+        # This executes the first REST API call and gets the response.
+        response = requests.request('POST', uri_base + '/vision/v1.0/RecognizeText', json=body, data=None,
+                                    headers=AZURE_REQ_HEADERS, params=params)
+
+        # Success is indicated by a status of 202.
+        if response.status_code != 202:
+            # if the first REST API call was not successful, display JSON data and exit.
+            parsed = json.loads(response.text)
+            print("Error:")
+            print(json.dumps(parsed, sort_keys=True, indent=2))
+            exit()
+
+        # The 'Operation-Location' in the response contains the URI to retrieve the recognized text.
+        operationLocation = response.headers['Operation-Location']
+
+        # Note: The response may not be immediately available. Handwriting recognition is an
+        # async operation that can take a variable amount of time depending on the length
+        # of the text you want to recognize. You may need to wait or retry this GET operation.
+
+        return operationLocation
+
+    except Exception as e:
+        print('Error:')
+        print(e)
+
+        ####################################
 
 
 if __name__ == '__main__':
     parse()
+    # app.run()
