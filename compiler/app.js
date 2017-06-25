f7c8695afe2bd8f450c0c166315b60b320093247
Carlin Au <carlinau@gmail.com>
2017-06-25 11:22:17 -0700

    remove quotes

--- compiler/app.js
+++ compiler/app.js
@@ -36,7 +36,8 @@ app.post('/payload', (req, res)=>{
   var body = req.body;
   console.log(req.body);
   console.log(body);
-  fs.writeFile(path.join(__dirname, '../dump/test.py'), unescape(body.data), (err)=>{
+  var resultdata = body.data.slice(1, -1);
+  fs.writeFile(path.join(__dirname, '../dump/test.py'), unescape(resultdata), (err)=>{
     if (err) {
       console.log(err);
     }
