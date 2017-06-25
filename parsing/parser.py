9af83e563d09cd6982739728b604e1b9b5c1e0f9
dchen <chendavid96@gmail.com>
2017-06-24 17:14:58 -0700

    committed and pushed on Sat Jun 24 17:14:58 PDT 2017

--- parsing/parser.py
+++ parsing/parser.py
@@ -32,9 +32,8 @@ except ImportError:
     from PIL import Image
 import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = '<full_path_to_your_tesseract_executable>'
+# pytesseract.pytesseract.tesseract_cmd = '<full_path_to_your_tesseract_executable>'
 # Include the above line, if you don't have tesseract executable in your PATH
 # Example tesseract_cmd: 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract'
 
-print(pytesseract.image_to_string(Image.open('test.png')))
-print(pytesseract.image_to_string(Image.open('test-european.jpg'), lang='fra'))
+print(pytesseract.image_to_string(Image.open('test_cases/function.jpg')))
